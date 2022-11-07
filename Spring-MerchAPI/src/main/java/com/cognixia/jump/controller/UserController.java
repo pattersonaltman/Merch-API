a4dae81fddc486f1a82212862a397f4d6ba26f3e
Patterson Alt <pattersonalt@Pattersons-MacBook-Pro-2.local>
2022-11-06 22:16:49 -0500

    added functionality to update User by id; (1) - [Modified] UserController, (2) - [Added] UpdateUserModel, (3) - [Added] UserService

--- Spring-MerchAPI/src/main/java/com/cognixia/jump/controller/UserController.java
+++ Spring-MerchAPI/src/main/java/com/cognixia/jump/controller/UserController.java
@@ -1,18 +1,23 @@
 package com.cognixia.jump.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cognixia.jump.model.UpdateUserModel;
 import com.cognixia.jump.model.User;
 import com.cognixia.jump.repository.UserRepository;
+import com.cognixia.jump.service.UserService;
 
 @RestController
 @RequestMapping("/api")
@@ -28,6 +33,12 @@ public class UserController {
 	
 	@Autowired
 	PasswordEncoder encoder;
+	
+	
+	
+	
+	@Autowired
+	UserService userServ;
 	
 	
 	
@@ -59,11 +70,53 @@ public class UserController {
 	
 	
 	
-	
+	// Get all users (Authorization: JWT)
 	@GetMapping("/user")
 	public List<User> getAllUsers() {
 		return userRepo.findAll();
 	}
+	
+	
+	
+	
+	// Get user by id
+	@GetMapping("/user/id/{id}")
+	public Optional<User> getById(@PathVariable Long id) {
+		return userRepo.findById(id);
+	}
+	
+	
+	
+	
+	// Get user by username
+	@GetMapping("/user/username/{username}")
+	public Optional<User> getByUsername(@PathVariable String username) {	
+		return userRepo.findByUsername(username);
+	}
+	
+	
+	
+	// Update user by id
+		/*
+		 * Potential Conflicts:
+		 * 	- 
+		 */
+	@PutMapping("/user/update")
+	public ResponseEntity<?> updateUser(@RequestBody UpdateUserModel model) {
+		
+		if(userServ.updateUserById(model))
+		{
+			return ResponseEntity.status(200).body(userRepo.findById(model.getIdToModify()));
+		}
+		
+		return ResponseEntity.status(400).body("User with id [" + model.getIdToModify() + "] does not exist");
+	}
+	
+	
+	
+	
+	
+	// Delete
 	
 	
 	
